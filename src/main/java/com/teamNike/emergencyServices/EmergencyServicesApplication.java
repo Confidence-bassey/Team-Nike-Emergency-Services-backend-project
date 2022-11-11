888ffb6a8265f9b04858503c9267a8773cf435b2
Confidence-bassey <confidencebass2@gmail.com>
2022-11-11 16:06:27 +0100

    teamNike emergency services backend projct first commit

---
+++ src/main/java/com/teamNike/emergencyServices/EmergencyServicesApplication.java
@@ -0,0 +1,13 @@
+package com.teamNike.emergencyServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamNike/emergencyServices/EmergencyServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamNike.emergencyServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
